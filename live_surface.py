fbaf50436788cbf8573ceeef2746ba6cb7b353f6
mherndon32 <mherndon32@gatech.edu>
2026-04-04 23:56:47 -0400

    Added application launch wrapper

--- live_surface.py
+++ live_surface.py
@@ -52,3 +52,58 @@ class LiveSurfaceApp(EClient, EWrapper):
     def tickOptionComputation(self, reqId, tickType, tickAttrib, impliedVol, delta, optPrice, pvDivedend, gamma, vega, theta, undPrice):
         if tickType == 13 and impliedVol is not None:
             self.iv_dict[reqId] = impliedVol
+
+
+def run_loop(app):
+    app.run()
+
+def start_app(symbol="SPY"):
+    app = LiveSurfaceApp()
+    app.connect('127.0.0.1', 7497, clientId=1)
+
+    api_thread = threading.Thread(target=run_loop, args = (app, ), daemon=True)
+    api_thread.start()
+    time.sleep(1)
+
+    underlying = Contract()
+    underlying.symbol = symbol
+    underlying.security = 'STK'
+    underlying.exchange = 'SMART'
+    underlying.currency = 'USD'
+
+    app.reqContractDetails(1, underlying)
+    app.resolve.wait(timeout = 5)
+    
+    #This number should be set to the reqId number you set up in tickPrice(), gets the mkt data (spot price) for the
+    app.reqMktData(999, underlying, "", False, False, [])
+    while app.spot_price == -1: time.sleep(.1)
+    spot = app.spot_price
+
+    app.reqSecDefOptParams(2, symbol, "", "STK", app.underlying_conId)
+    app.chain_resolved.wait(timeout=5)
+
+    today = time.strftime("%Y%m%d")
+    target_exps = [e for e in app.expirations if e >= today][:6]
+    target_strikes = [s for s in app.strikes if spot *.98 <= s <= spot *1.02]
+
+    req_id = 1000
+    #Creates the live data streams for the options contracts that we requested directly above in target_exps and target_strikes
+    for exp in target_exps:
+        for strike in target_strikes:
+            opt = Contract()
+            opt.symbol = symbol
+            opt.secType = 'OPT'
+            opt.exchange = 'SMART'
+            opt.currency = 'USD'
+            opt.lastTradeDateOrContractMonth = exp
+            opt.strike = strike
+            opt.right = 'C' if strike >= spot else 'P'
+            app.id_map[req_id] = (exp, strike)
+
+            app.reqMktData(req_id, opt, "106", False, False, [])
+            req_id += 1
+            time.sleep(.1)
+    return app
+
+
+
